a669812f3c4148b3bcca019e281d33a7f32c5bb4
Luis Weich <mambabosso@gmail.com>
2020-03-10 20:45:58 +0100

    Update

--- src/main/java/com/github/mambabosso/starterkit/Starterkit.java
+++ src/main/java/com/github/mambabosso/starterkit/Starterkit.java
@@ -1,6 +1,8 @@
 package com.github.mambabosso.starterkit;
 
+import com.github.mambabosso.starterkit.health.DatabaseHealthCheck;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public final class Starterkit extends Application<StarterkitConfiguration> {
@@ -13,8 +15,24 @@ public final class Starterkit extends Application<StarterkitConfiguration> {
         }
     }
 
+    private Bootstrap<StarterkitConfiguration> bootstrap;
+    private Environment environment;
+    private StarterkitConfiguration configuration;
+
+    @Override
+    public void initialize(final Bootstrap<StarterkitConfiguration> bootstrap) {
+        this.bootstrap = bootstrap;
+    }
+
     @Override
     public void run(final StarterkitConfiguration starterkitConfiguration, final Environment environment) throws Exception {
+        this.environment = environment;
+        this.configuration = configuration;
+        registerHealthChecks();
+    }
+
+    private void registerHealthChecks() {
+        environment.healthChecks().register("DatabaseHealthCheck", new DatabaseHealthCheck());
     }
 
 }
